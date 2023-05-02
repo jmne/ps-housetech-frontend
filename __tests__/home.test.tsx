09cddec4941b99ca15110403a3e95c0535e1f2e5
sbiel <simonbiel@uni-muenster.de>
2023-05-02 15:03:08 +0200

    Created initial NextJS setup with Typescript, Jest & SCSS

---
+++ __tests__/home.test.tsx
@@ -0,0 +1,11 @@
+import Home from "@/pages/home";
+import "@testing-library/jest-dom";
+import { fireEvent, render, screen } from "@testing-library/react";
+
+describe("Homepage", () => {
+    it("renders a sample page", () => {
+        render(<Home/>);
+        // Check for test string
+        expect(screen.getByText("Test: Homepage")).toBeInTheDocument();
+    });
+});
