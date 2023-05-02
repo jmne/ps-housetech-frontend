0fc9c9e2c9941199ce833bb653e4b6e52276f7cb
Jan <jmenne@uni-muenster.de>
2023-05-02 20:28:26 +0200

    Add pre-commit hooks

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,7 +1,5 @@
-import styles from '@/pages/index.module.scss'
+import styles from "@/pages/index.module.scss";
 
 export default function Home() {
-  return (
-    <p>Test: Homepage</p>
-  )
+  return <p>Test: Homepage</p>;
 }
