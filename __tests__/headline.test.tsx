7e779fd177c8165c3dff3871eb47f0cb12dd606e
Kevin Schulz <kschulz6@uni-muenster.de>
2023-05-17 01:30:41 +0200

    modified headline.test.tsx

--- __tests__/headline.test.tsx
+++ __tests__/headline.test.tsx
@@ -3,12 +3,12 @@ import "@testing-library/jest-dom";
 import { fireEvent, render, screen } from "@testing-library/react";
 
 describe("Homepage", () => {
-    it("renders a sample page", () => {
-      render(<Headline />);
-      // Check for test string
-      expect(screen.getByText("Department of Information Systems")).toBeInTheDocument();
-      // Check for images
-      expect(screen.getByAltText("WWU Logo")).toBeInTheDocument();
-      expect(screen.getByAltText("ERCIS Logo")).toBeInTheDocument();
-    });
+  it("renders a sample page", () => {
+    render(<Headline />);
+    // Check for test string
+    expect(screen.getByText("Department of Information Systems")).toBeInTheDocument();
+    // Check for images
+    expect(screen.getByAltText("WWU Logo")).toBeInTheDocument();
+    expect(screen.getByAltText("ERCIS Logo")).toBeInTheDocument();
   });
+});
