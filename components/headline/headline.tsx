4f4aa1f75d0894e02d482e019a0a554574c967d7
sbiel <simonbiel@uni-muenster.de>
2023-05-17 15:15:12 +0200

    WWU logo now without text

--- components/headline/headline.tsx
+++ components/headline/headline.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import styles from "@/components/Headline/Headline.module.scss";
 
 // IMPORTS - ICONS
-import logoWWU from "assets/images/wwu.svg";
+import logoWWU from "assets/images/wwu-no-text.svg";
 import logoERCIS from "assets/images/ercis.svg";
 
 /**
@@ -16,12 +16,12 @@ export default function Headline() {
   return (
     <div className={styles.headlineWrapper}>
       <div className={styles.logo}>
-        <Image src={logoWWU} alt="WWU Logo" fill={false} className={styles.logo} />
+        <Image src={logoWWU} alt="WWU Logo" fill={false} className={[styles.logo, styles.wwu].join(" ")} />
       </div>
       <div className={styles.title}>
         <h1>Department of Information Systems</h1>
       </div>
-      <div className={[styles.logo, styles.alignRight].join(" ")}>
+      <div className={styles.logo}>
         <Image src={logoERCIS} alt="ERCIS Logo" fill={false} className={styles.logo} />
       </div>
     </div>
