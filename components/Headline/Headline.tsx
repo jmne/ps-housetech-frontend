6136822e0d2ffcbc772bce79d681bb91e763fd7b
sbiel <simonbiel@uni-muenster.de>
2023-05-21 19:16:14 +0200

    New language selector added

--- components/Headline/Headline.tsx
+++ components/Headline/Headline.tsx
@@ -1,23 +1,15 @@
 // IMPORTS - BUILTINS
 import Image from "next/image";
-import Link from "next/link";
-import { useRouter } from "next/router";
 import { useTranslation } from "next-i18next";
+
+// IMPORTS - COMPONENTS
+import { LanguageSelector } from "./LanguageSelector/LanguageSelector";
 
 // IMPORTS - ASSETS
 import styles from "@/components/Headline/Headline.module.scss";
-
-// IMPORTS - ICONS
 import logoWWU from "assets/images/wwu-no-text.svg";
 import logoERCIS from "assets/images/ercis.svg";
-import flag_de from "assets/images/flag/flag_germany.png";
-import flag_uk from "assets/images/flag/flag_uk.png";
-import { useEffect } from "react";
 
-function flagPath(lang: string | undefined){
-  if (lang === "de") return flag_uk
-  else return flag_de
-}
 
 /**
  *
@@ -25,13 +17,6 @@ function flagPath(lang: string | undefined){
 */
 export default function Headline() {
   const { t } = useTranslation('index')
-  const router = useRouter()
-  const currentLang = router.locale
-  
-  function switchLang(){
-    if (currentLang === "en") router.push("/", "/", {locale: "de"})
-    else router.push("/", "/", {locale: "en"})
-  }
   
   return (
     <div className={styles.headlineWrapper}>
@@ -43,7 +28,7 @@ export default function Headline() {
         <h1>{t('header.title')}</h1>
       </div>
       <div className={[styles.side, styles.right].join(" ")}>
-        <Image alt="Flag" src={flagPath(currentLang)} fill={false} className={styles.flagButton} onClick={switchLang}/>
+        <LanguageSelector />
       </div>
     </div>
   );
