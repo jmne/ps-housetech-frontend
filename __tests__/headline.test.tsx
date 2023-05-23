a392d41a9871d24cbbb70221b123a64d44862b51
Simon Maximilian Biel <simonbiel@uni-muenster.de>
2023-05-23 19:22:02 +0200

    Dev multi language

--- __tests__/headline.test.tsx
+++ __tests__/headline.test.tsx
@@ -1,12 +1,24 @@
-import Headline from "@/components/Headline/Headline";
+// Testing library
 import "@testing-library/jest-dom";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
-describe("Homepage", () => {
-  it("renders a sample page", () => {
-    render(<Headline />);
+// Translation Setup
+import i18n from "../i18-test_config";
+import { I18nextProvider } from "react-i18next";
+
+// Component
+import Headline from "@/components/Headline/Headline";
+
+describe("Headline", () => {
+  it("Renders the headline", () => {
+    render(
+      <I18nextProvider i18n={i18n}>
+        <Headline />
+      </I18nextProvider>
+    );
     // Check for test string
     expect(screen.getByText("Department of Information Systems")).toBeInTheDocument();
+    
     // Check for images
     expect(screen.getByAltText("WWU Logo")).toBeInTheDocument();
     expect(screen.getByAltText("ERCIS Logo")).toBeInTheDocument();
