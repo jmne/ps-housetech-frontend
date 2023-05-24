019ed2a54d393daa089dbe4d3ec182208216a2f5
sbiel <simonbiel@uni-muenster.de>
2023-05-24 14:31:08 +0200

    Fixed scrolling & shadows

--- components/Cafeteriaplan/Cafeteriaplan.tsx
+++ components/Cafeteriaplan/Cafeteriaplan.tsx
@@ -12,17 +12,17 @@ import { DishInformation } from "types/DishInformation";
 import { useTranslation } from "next-i18next";
 
 export default function Cafeteriaplan() {
-  const { t }=useTranslation("index");
+  const { t } = useTranslation("index");
   const { data, isLoading, error } = useCafeteriaplan();
 
   return (
     <section
-      className={[indexStyles.smallContainer, indexStyles.contentSection].join(" ")}
+      className={[indexStyles.smallContainer, indexStyles.contentSection, cafeteriaStyles.shadowFix].join(" ")}
     >
-      <div className={indexStyles.cardHeadline}>
+      <div className={[indexStyles.cardHeadline, cafeteriaStyles.headlineMargin].join(" ")}>
         <h2>{t("cafeteria_plan.title")}</h2>
       </div>
-      <ol className={cafeteriaStyles.cafeteriaplan}>
+      <ol className={[cafeteriaStyles.cafeteriaplan, cafeteriaStyles.scrolling].join(" ")}>
         {data.map((dish, index) => (
           <Dish dish={dish} key={index} />
         ))}
