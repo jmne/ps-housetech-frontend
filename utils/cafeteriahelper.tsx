604adc6d3e744d95ae3516e35d281bde18e4e0ad
kschulz6@uni-muenster.de <kschulz6@uni-muenster.de>
2023-06-19 17:44:10 +0200

    added formatFoodplan function

--- utils/cafeteriahelper.tsx
+++ utils/cafeteriahelper.tsx
@@ -1,16 +1,19 @@
 import { Foodplan } from "types/Foodplan";
 
 export function getCafeteriaDateString(date: Date) {
-    const month_short = date.getMonth();
-    const day_short = date.getDate();
-  
-    const year = date.getFullYear();
-    const month = month_short.valueOf() < 10 ? `0${month_short}` : month_short;
-    const day = day_short.valueOf() < 10 ? `0${day_short}` : day_short;
-  
-    return `${year}-${month}-${day}`;
-  }
+  const month_short = date.getMonth();
+  const day_short = date.getDate();
 
-  export function getIndexForDate(data: Foodplan[], date: Date) {
-    const formattedDate = getCafeteriaDateString(date);
-  }
+  const year = date.getFullYear();
+  const month = month_short.valueOf() < 10 ? `0${month_short}` : month_short;
+  const day = day_short.valueOf() < 10 ? `0${day_short}` : day_short;
+
+  return `${year}-${month}-${day}`;
+}
+
+export function getIndexForDate(data: Foodplan[], date: Date) {
+  const formattedDate = getCafeteriaDateString(date);
+  const index = data.findIndex((element) => element.date == formattedDate);
+}
+
+export function createNewFoodplanArray(data: Foodplan[]) {}
