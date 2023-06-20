b56d2b04ecc3c15656b8eca4a8a60fd761aded89
sbiel <simonbiel@uni-muenster.de>
2023-06-20 16:44:30 +0200

    Fixed keyboard bug after merge

--- components/News/News.tsx
+++ components/News/News.tsx
@@ -1,37 +1,29 @@
 import { Events } from "./Events/Events";
 
-import indexStyles from "@/pages/index.module.scss"
-import styles from "./News.module.scss"
+import indexStyles from "@/pages/index.module.scss";
+import styles from "./News.module.scss";
 import { useTranslation } from "next-i18next";
 import { useSearchInputContext } from "context/SearchInputContext";
-import { useState, useRef, useEffect } from "react";
 import Keyboard from "@/components/Keyboard/Keyboard";
 
 export function News() {
-    const { t } = useTranslation("index")
+  const { t } = useTranslation("index");
+  const searchInputContext = useSearchInputContext();
 
-    const [activeKeyboard, setActiveKeyboard] = useState(false);
-    const searchInputContext = useSearchInputContext();
-    const keyboardRef = useRef<HTMLElement>();
+  return (
+    <section
+      className={[
+        indexStyles.contentSection,
+        indexStyles.newsContainer,
+        styles.container
+      ].join(" ")}
+    >
+      <div className={styles.headlineContainer}>
+        <h2>{t("news.events.title")}</h2>
+      </div>
+      <Events />
 
-    useEffect(() => {
-        if (searchInputContext.active) setActiveKeyboard(true);
-        else {
-            if (keyboardRef.current) keyboardRef.current.style.opacity = "0";
-            setTimeout(() => {
-                setActiveKeyboard(false);
-            }, 500);
-        }
-    }, [searchInputContext.active]);
-
-    return (
-        <section className={[indexStyles.contentSection, indexStyles.newsContainer, styles.container].join(" ")} >
-            <div className={styles.headlineContainer}>
-                <h2>{t("news.events.title")}</h2>
-            </div>
-            <Events />
-
-            {activeKeyboard ? <Keyboard ref={keyboardRef} /> : <></>}
-        </section>
-    )
+      <Keyboard visible={searchInputContext.active} />
+    </section>
+  );
 }
