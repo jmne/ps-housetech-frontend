2c29413d31438fcad34d8ae95541157fe18420ed
sbiel <simonbiel@uni-muenster.de>
2023-06-24 14:10:48 +0200

    Refactoring in Controls.tsx

--- components/Wayfinder/Map/Controls.tsx
+++ components/Wayfinder/Map/Controls.tsx
@@ -65,10 +65,15 @@ function handleHighlightLeo11(mapContext: MapData) {
   }
 }
 
-function handleTouchEnd(touchStart: number, touchEnd: number, mapContext: MapData) {
+function handleTouchEnd(
+  touchStart: number | undefined,
+  touchEnd: number,
+  mapContext: MapData
+) {
   if (
     mapContext.current.area !== "leo3" ||
-    typeof mapContext.current.floor === "undefined"
+    typeof mapContext.current.floor === "undefined" ||
+    typeof touchStart === "undefined"
   )
     return;
 
@@ -91,88 +96,40 @@ export function Controls() {
   const selectedPersonContext = usePersonSearchContext();
 
   const [touchStart, setTouchStart] = useState<number | undefined>();
-  const handleTouchEnd = (touchEnd: number) => {
-    debugger;
-    if (
-      mapContext.current.area !== "leo3" ||
-      typeof mapContext.current.floor === "undefined"
-    )
-      return;
 
-    if (typeof touchStart !== "number") return;
-    const goUp = touchStart - touchEnd < 0;
-
-    if (goUp) {
-      const nextFloorIndex = getFloorIndex(mapContext.current.floor) + 1;
-      if (nextFloorIndex > 3) return;
-      mapContext.setCurrent({ floor: `floor${nextFloorIndex}` });
-    } else {
-      const nextFloorIndex = getFloorIndex(mapContext.current.floor) - 1;
-      if (nextFloorIndex < 0) return;
-      mapContext.setCurrent({ floor: `floor${nextFloorIndex}` });
-    }
-  };
-
-  if (mapContext.current.area === buildingNames.CAMPUS) {
-    return (
-      <div
-        className={styles.controls}
-        onMouseDown={(e) => {
-          setTouchStart(e.pageY);
-        }}
-        onMouseUp={(e) => {
-          handleTouchEnd(e.pageY);
-        }}
-      >
+  return (
+    <div
+      className={styles.controls}
+      onMouseDown={(e) => {
+        setTouchStart(e.pageY);
+      }}
+      onMouseUp={(e) => {
+        handleTouchEnd(touchStart, e.pageY, mapContext);
+      }}
+    >
+      {mapContext.current.area !== buildingNames.LEO3 && (
         <button onClick={() => handleShowLeo3(mapContext, selectedPersonContext)}>
           Show Leo 3
         </button>
-        <button onClick={() => handleHighlightLeo3(mapContext)}>Where is Leo 3?</button>
+      )}
+      {mapContext.current.area !== buildingNames.LEO11 && (
         <button onClick={() => handleShowLeo11(mapContext, selectedPersonContext)}>
           Show Leo 11
         </button>
-        <button onClick={() => handleHighlightLeo11(mapContext)}>Where is Leo 11?</button>
-      </div>
-    );
-  } else if (mapContext.current.area === buildingNames.LEO3) {
-    return (
-      <div
-        className={styles.controls}
-        onMouseDown={(e) => {
-          setTouchStart(e.pageY);
-        }}
-        onMouseUp={(e) => {
-          handleTouchEnd(e.pageY);
-        }}
-      >
+      )}
+      {mapContext.current.area !== buildingNames.CAMPUS && (
         <button onClick={() => handleBackToCampus(mapContext, selectedPersonContext)}>
           Back to Campus
         </button>
-        <button onClick={() => handleShowLeo11(mapContext, selectedPersonContext)}>
-          Show Leo 11
-        </button>
-      </div>
-    );
-  } else if (mapContext.current.area === buildingNames.LEO11) {
-    return (
-      <div
-        className={styles.controls}
-        onMouseDown={(e) => {
-          setTouchStart(e.pageY);
-        }}
-        onMouseUp={(e) => {
-          handleTouchEnd(e.pageY);
-        }}
-      >
-        <button onClick={() => handleBackToCampus(mapContext, selectedPersonContext)}>
-          Back to Campus
-        </button>
-        <button onClick={() => handleShowLeo3(mapContext, selectedPersonContext)}>
-          Show Leo 3
-        </button>
-      </div>
-    );
-  }
-
-  return <></>;
+      )}
+      {mapContext.current.area === buildingNames.CAMPUS && (
+        <>
+          <button onClick={() => handleHighlightLeo3(mapContext)}>Where is Leo 3?</button>
+          <button onClick={() => handleHighlightLeo11(mapContext)}>
+            Where is Leo 11?
+          </button>
+        </>
+      )}
+    </div>
+  );
 }
