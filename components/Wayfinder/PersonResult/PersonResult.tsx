00cc072f033aa0bda00643d10be920b48cadb73d
sbiel <simonbiel@uni-muenster.de>
2023-06-29 15:39:05 +0200

    Profile images with lazy loading added

--- components/Wayfinder/PersonResult/PersonResult.tsx
+++ components/Wayfinder/PersonResult/PersonResult.tsx
@@ -5,6 +5,8 @@ import { usePersonSearchContext } from "context/PersonContext";
 import styles from "@/components/Wayfinder/Wayfinder.module.scss";
 import { Fragment, useEffect, useRef } from "react";
 import { handleClickOnPerson } from "utils/Wayfinder/personCardsTransformations";
+import Image from "next/image";
+import { fetchImage } from "requests/fetchImage";
 
 interface props {
   person: Employee;
@@ -12,7 +14,7 @@ interface props {
 
 export const SEARCH_RESULT_COLLAPSED = styles.person;
 export const SEARCH_RESULT_EXPANDED = [styles.person, styles.expanded].join(" ");
-
+const url = "https://ps-housetech.uni-muenster.de:444/api/picture/";
 function PersonResult({ person }: props) {
   const { t } = useTranslation("index");
   const mapContext = useMapContext();
@@ -31,8 +33,23 @@ function PersonResult({ person }: props) {
       onClick={() => handleClickOnPerson(person, mapContext, selectedPersonContext)}
       ref={personRef}
     >
-      <span>{`${person.cfFirstNames} ${person.cfFamilyNames}`}</span>
-      <span className={styles.caption}>{person.chair}</span>
+      <div className={styles.header}>
+        <div className={styles.imageWrapper}>
+          {person.image ? (
+            <Image
+              src={`${url}${person.image}`}
+              alt="profile picture"
+              sizes="100px"
+              style={{ objectFit: "cover" }}
+              fill
+            />
+          ) : undefined}
+        </div>
+        <div className={styles.shortInfo}>
+          <span>{`${person.cfFirstNames} ${person.cfFamilyNames}`}</span>
+          <span className={styles.caption}>{person.chair}</span>
+        </div>
+      </div>
       <div className={styles.hidden}>
         {person.phones.map((phoneNumer, index) => {
           return (
@@ -52,6 +69,13 @@ function PersonResult({ person }: props) {
             </Fragment>
           );
         })}
+        {person.roomNumber && (
+          <>
+            <span className={styles.attribute}>Room</span>
+            <span className={styles.caption}>{person.roomNumber}</span>
+            <br />
+          </>
+        )}
       </div>
     </li>
   );
--- components/Wayfinder/Wayfinder.tsx
+++ components/Wayfinder/Wayfinder.tsx
@@ -98,10 +98,10 @@ export function Wayfinder() {
     )
       return;
 
-    const scroll_by = contextPersonElement.offsetTop - listRef.current.scrollTop - 5;
-    requestAnimationFrame(() =>
-      listRef.current?.scrollBy({ top: scroll_by, behavior: "smooth" })
-    );
+    requestAnimationFrame(() => {
+      const scroll_by = contextPersonElement.offsetTop - listRef.current.scrollTop - 5;
+      listRef.current?.scrollBy({ top: scroll_by, behavior: "smooth" });
+    });
   }, [selectedPersonContext.current_person]);
 
   return (
