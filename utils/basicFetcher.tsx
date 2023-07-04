4de783cdc15730548d8ebd5b575268f5c8d7e747
sbiel <simonbiel@uni-muenster.de>
2023-07-04 12:51:03 +0200

    Fixed TypeScript error by adding new ApiError type

--- utils/basicFetcher.tsx
+++ utils/basicFetcher.tsx
@@ -1,8 +1,10 @@
+import { ApiError } from "types/Error";
+
 // Fetcher to be used in all fetch functions
 export const fetcher = (url: string) =>
   fetch(url).then((res) => {
     if (!res.ok) {
-      const error = new Error("An error occurred while fetching the data.");
+      const error: ApiError = new Error("An error occurred while fetching the data.");
       // Attach extra info to the error object.
       error.info = res.json();
       error.status = res.status;
