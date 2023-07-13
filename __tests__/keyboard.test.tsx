6e6beb4238556d81b1927f753f975d4b9b08af14
sbiel <simonbiel@uni-muenster.de>
2023-07-13 22:00:58 +0200

    Fixed tests

--- __tests__/keyboard.test.tsx
+++ __tests__/keyboard.test.tsx
@@ -19,7 +19,6 @@ describe("Keyboard and Key components", () => {
       ["a", "s", "d", "f", "g", "h", "j", "k", "l", "ö", "ä"],
       ["y", "x", "c", "v", "b", "n", "m", "-"]
     ];
-    const special_keys = ["clear", "space", "backspace"];
 
     render(<Keyboard visible/>);
 
@@ -27,9 +26,6 @@ describe("Keyboard and Key components", () => {
       row.forEach((value) => {
         expect(screen.getByText(value.toUpperCase())).toBeInTheDocument();
       });
-    });
-    special_keys.forEach((key) => {
-      expect(screen.getByText(key)).toBeInTheDocument();
     });
   });
 
@@ -45,34 +41,6 @@ describe("Keyboard and Key components", () => {
     fireEvent.mouseDown(getByText(/A/));
 
     expect(setInput).toHaveBeenCalledWith("A");
-  });
-
-  test("Key component handles 'backspace' correctly", () => {
-    const setInput = jest.fn();
-    useSearchInputContext.mockReturnValue({
-      active: true,
-      input: "A",
-      setInput
-    });
-
-    const { getByText } = render(<Key keycode="backspace" />);
-    fireEvent.mouseDown(getByText(/backspace/));
-
-    expect(setInput).toHaveBeenCalledWith("");
-  });
-
-  test("Key component handles 'clear' correctly", () => {
-    const setInput = jest.fn();
-    useSearchInputContext.mockReturnValue({
-      active: true,
-      input: "A",
-      setInput
-    });
-
-    const { getByText } = render(<Key keycode="clear" />);
-    fireEvent.mouseDown(getByText(/clear/));
-
-    expect(setInput).toHaveBeenCalledWith("");
   });
 
   test("Key component handles 'space' correctly", () => {
