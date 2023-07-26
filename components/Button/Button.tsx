f69216553c9c71cc1ec703e5c16b7cd005da5acb
sbiel <simonbiel@uni-muenster.de>
2023-07-26 15:02:39 +0200

    Fixed forwardRef in Button

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,30 +1,29 @@
-import { ButtonHTMLAttributes, DetailedHTMLProps, LegacyRef, forwardRef } from "react";
+import { ButtonHTMLAttributes, DetailedHTMLProps, forwardRef } from "react";
 import styles from "./Button.module.scss";
 
-interface props
+interface Props
   extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
   iconSize?: "s" | "m";
   smallPadding?: boolean;
 }
 
-export const Button = forwardRef(
-  (
-    { children, className, smallPadding, iconSize, ...props }: props,
-    ref: LegacyRef<HTMLButtonElement>
-  ) => {
-    return (
-      <button
-        className={[
-          styles.base,
-          iconSize === "m" ? styles.iconMedium : undefined,
-          smallPadding ? styles.smallPadding : undefined,
-          className
-        ].join(" ")}
-        ref={ref ? ref : undefined}
-        {...props}
-      >
-        {children}
-      </button>
-    );
-  }
+const Button = forwardRef<HTMLButtonElement, Props>(
+  ({ children, className, smallPadding, iconSize, ...props }, ref) => (
+    <button
+      className={[
+        styles.base,
+        iconSize === "m" ? styles.iconMedium : undefined,
+        smallPadding ? styles.smallPadding : undefined,
+        className
+      ].join(" ")}
+      ref={ref}
+      {...props}
+    >
+      {children}
+    </button>
+  )
 );
+
+Button.displayName = "Button";
+
+export default Button;
