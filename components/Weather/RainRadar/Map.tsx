57f3deb7af544f08f76b08d56636148c8244d831
sbiel <simonbiel@uni-muenster.de>
2023-07-26 20:08:24 +0200

    Working with react-leaflet without refresh

--- components/Weather/RainRadar/Map.tsx
+++ components/Weather/RainRadar/Map.tsx
@@ -6,21 +6,21 @@ const rain_layer_url = `https://tile.openweathermap.org/map/precipitation_new/{z
 
 export default function Map() {
   return (
-      <MapContainer
-        center={locationMuenster}
-        zoom={8}
-        scrollWheelZoom={false}
-        attributionControl={false}
-        zoomControl={false}
-        dragging={false}
-        doubleClickZoom={false}
-        boxZoom={false}
-        trackResize={false}
-        closePopupOnClick={false}
-      >
-        <TileLayer url={rain_layer_url} updateInterval={10} />
-        <TileLayer url={"https://tile.openstreetmap.org/{z}/{x}/{y}.png"} />
-        <Marker position={[51.96, 7.628]} />
-      </MapContainer>
+    <MapContainer
+      center={locationMuenster}
+      zoom={8}
+      scrollWheelZoom={false}
+      attributionControl={false}
+      zoomControl={false}
+      dragging={false}
+      doubleClickZoom={false}
+      boxZoom={false}
+      trackResize={false}
+      closePopupOnClick={false}
+    >
+      <TileLayer url={rain_layer_url} />
+      <TileLayer url={"https://tile.openstreetmap.org/{z}/{x}/{y}.png"} />
+      <Marker position={[51.96, 7.628]} />
+    </MapContainer>
   );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -28,47 +28,28 @@ import { SearchInputProvider } from "context/SearchInputContext";
 import { TimeoutProvider } from "context/TimeoutContext";
 import { ToastProvider } from "@/components/Toast/ToastProvider";
 import { ToastContextProvider } from "context/ToastContext";
-import Head from "next/head";
-import Script from "next/script";
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <>
-    <Head>
-        <link
-          rel="stylesheet"
-          href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-          integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
-          crossOrigin=""
-        />
-    </Head>
-    <Script>
-      <script
-        src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-        integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
-        crossOrigin=""
-      ></script>
-    </Script>
-    <ToastContextProvider>
-      <ToastProvider>
-        <TimeoutProvider>
-          <SearchInputProvider>
-            <main className={productSans.className} id="app-wrapper">
-              <style jsx global>{`
-                :root {
-                  /* ... */
-                  --product-font: ${productSans.style.fontFamily};
-                }
-                html * {
-                  font-family: var(--product-font) !important;
-                }
-              `}</style>
-              <Component {...pageProps} />
-            </main>
-          </SearchInputProvider>
-        </TimeoutProvider>
-      </ToastProvider>
-    </ToastContextProvider>
-  </>
+  <ToastContextProvider>
+    <ToastProvider>
+      <TimeoutProvider>
+        <SearchInputProvider>
+          <main className={productSans.className} id="app-wrapper">
+            <style jsx global>{`
+              :root {
+                /* ... */
+                --product-font: ${productSans.style.fontFamily};
+              }
+              html * {
+                font-family: var(--product-font) !important;
+              }
+            `}</style>
+            <Component {...pageProps} />
+          </main>
+        </SearchInputProvider>
+      </TimeoutProvider>
+    </ToastProvider>
+  </ToastContextProvider>
 );
 
 export default appWithTranslation(MyApp);
