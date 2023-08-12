6befa1eb2e1eae137d6dc64408199a9e50ae4f4a
Simon Maximilian Biel <simonbiel@uni-muenster.de>
2023-08-12 14:50:54 +0200

    Resolve "Search hold backwards doesn't work"

---
+++ components/Keyboard/Keys/Clear.tsx
@@ -0,0 +1,25 @@
+import styles from "@/components/Keyboard/Keyboard.module.scss";
+import IconDelete from "assets/icons/delete.svg";
+import { useSearchInputContext } from "context/SearchInputContext";
+import React, { memo, useCallback } from "react";
+
+function Clear() {
+  const searchContext = useSearchInputContext();
+
+  const clearInput = useCallback(
+    (e: React.MouseEvent) => {
+      if (!searchContext.active) return;
+      searchContext.setInput("");
+      e.preventDefault();
+    },
+    [searchContext]
+  );
+
+  return (
+    <button className={styles.key} onMouseDown={(e) => clearInput(e)}>
+      <IconDelete className={styles.icon} />
+    </button>
+  );
+}
+
+export default memo(Clear);
---
+++ components/Keyboard/Keys/index.tsx
@@ -0,0 +1,6 @@
+import Backspace from "./Backspace";
+import Clear from "./Clear";
+import Space from "./Space";
+import Letter from "./Letter";
+
+export { Backspace, Clear, Space, Letter };
