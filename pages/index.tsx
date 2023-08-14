5a31887da6ed4e717c4b0252e1494f87e799ea9a
Simon Maximilian Biel <simonbiel@uni-muenster.de>
2023-08-14 10:46:58 +0200

    Added error-boundaries to all elements

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,30 +2,29 @@
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 // IMPORTS - COMPONENTS
-import Headline from "@/components/Dashboard/Headline/Headline";
-import Busplan from "@/components/Dashboard/Busplan/Busplan";
-import Cafeteriaplan from "@/components/Dashboard/Cafeteriaplan/Cafeteriaplan";
 
 // IMPORTS - ASSETS
 import styles from "@/pages/index.module.scss";
-import { Wayfinder } from "@/components/Dashboard/Wayfinder/Wayfinder";
 
 import "swiper/css";
 import "swiper/css/navigation";
 
 // IMPORTS - CONTEXT
-import { SelectedPersonProvider } from "context/PersonContext";
-import { Weather } from "@/components/Dashboard/Weather/Weather";
-import { News } from "@/components/Dashboard/News/News";
+import {
+  Headline,
+  Wayfinder,
+  Weather,
+  Cafeteriaplan,
+  News,
+  Busplan
+} from "@/components/Dashboard";
 
 export default function Index() {
   return (
     <div className={styles.wrapper} id="index-wrapper">
       <Headline />
       <div className={styles.bodyWrapper}>
-        <SelectedPersonProvider>
-          <Wayfinder />
-        </SelectedPersonProvider>
+        <Wayfinder />
         <Weather />
         <Cafeteriaplan />
         <News />
