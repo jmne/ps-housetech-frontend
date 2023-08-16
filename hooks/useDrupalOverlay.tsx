0e435a1eb12d4931989c39f7a8747bfb9aaa3cac
sbiel <simonbiel@uni-muenster.de>
2023-08-16 23:45:49 +0200

    Live data implemented

--- hooks/useDrupalOverlay.tsx
+++ hooks/useDrupalOverlay.tsx
@@ -4,7 +4,6 @@ import { fetcher } from "utils/basicFetcher";
 
 // IMPORTS - ASSETS
 import { Overlay } from "types/Drupal";
-import { sampleOverlay } from "types/Drupal";
 
 const url = "https://ps-housetech.uni-muenster.de:444/api/drupal/overlay";
 const options = { refreshInterval: 60 * 1000 };
@@ -12,5 +11,5 @@ const options = { refreshInterval: 60 * 1000 };
 export default function useDrupalOverlay(locale?: string) {
   const { data, isLoading, error } = useSWR<Overlay[]>(url, fetcher, options);
 
-  return { data: sampleOverlay, isLoading, error };
+  return { data, isLoading, error };
 }
